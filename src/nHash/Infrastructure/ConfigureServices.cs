19369c60857bfeb27ca320673c4886c7a2d3b700
Naser <naser.r@gmail.com>
2023-03-20 18:48:12 +0330

    Remove ILogger

--- src/nHash/Infrastructure/ConfigureServices.cs
+++ src/nHash/Infrastructure/ConfigureServices.cs
@@ -1,12 +1,9 @@
-using nHash.Infrastructure.Helper.Logging;
-
 namespace nHash.Infrastructure;
 
 public static class ConfigureServices
 {
     public static void Register(IServiceCollection services)
     {
-        services.AddLoggingExt();
         services.AddSingleton<IFileProvider, FileProvider>();
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         services.AddSingleton<IOutputProvider, OutputProvider>();
