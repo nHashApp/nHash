1bd6cd57968c7febd8f8efd19a179e5572f445a9
Naser <naser.r@gmail.com>
2023-03-23 03:24:55 +0330

    Add conversion between JSON, YAML, and XML
    Can convert any of these types to each other

--- src/nHash/Program.cs
+++ src/nHash/Program.cs
@@ -1,18 +1,6 @@
 ﻿using nHash.App;
 using nHash.Application.Shared.Conversions;
 
-var source = """
-<?xml version="1.0" encoding="ISO-8859-1"?>  
-        <note>  
-    <to>Tove</to>  
-    <from>Jani</from>  
-    <heading>Reminder</heading>  
-    <body>Don't forget me this weekend!</body>  
-    </note> 
-""";
-var target= Conversion.ToYaml(source, ConversionType.XML);
-Console.WriteLine(target);
-
 var services = new ServiceCollection();
 Startup.RegisterServices(services);
 var provider = services.BuildServiceProvider();
